ac2b05669b961e824d2551a5534991b3c1c35cb7
unset <unset>
2025-05-14 12:56:38 -0300

    Ajuste no carrosel do idolos e Criação da seção do estádio

--- web-data-viz-main/public/js/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarTexto(id) {
-    var texto = id.value;
-    if (texto.style.display === "none") {
-        texto.style.display = "block";
-    } else {
-        texto.style.display = "none";
-    }
-}
