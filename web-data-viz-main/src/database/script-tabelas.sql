b7e4cf96d746356d0eb0a78e9932ceb44fa7039b
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-05-27 21:59:33 -0300

    Alteração na nomenclatura das colunas

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -1,66 +1,3 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
-
 -- database para o projeto individual
 create database ProjetoIndividual;
 
@@ -76,15 +13,15 @@ index ix_usuario (idusuario)
 
 create table perguntaQuiz (
   id SERIAL PRIMARY KEY,
-  texto TEXT NOT NULL,
-  tipo VARCHAR(50) DEFAULT 'unica',
+  textoPergunta TEXT NOT NULL,
+  tipoPergunta VARCHAR(50) DEFAULT 'unica',
   ordem INT
 );
 
-
 create table respostaQuiz (
   id SERIAL PRIMARY KEY,
-  id_pergunta INT NOT NULL REFERENCES perguntas(id) ON DELETE CASCADE,
-  texto TEXT NOT NULL,
-  ordem INT
+  fkpergunta INT NOT NULL,
+  textoResposta TEXT NOT NULL,
+  ordem INT,
+  constraint fk_pergunta foreign key (fkpergunta) references perguntaQuiz(id)
 );
